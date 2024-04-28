59b673fb9f8da91c059bf6f6d793491f6f13b431
Yuxiang Yang <yangyuxiang@szkingdom.com>
2024-04-28 17:13:46 +0800

    feat: 新增任务队列，实现多次触发响应式数据，但只更新一次

--- index.js
+++ index.js
@@ -71,6 +71,34 @@ function cleanup(effectFn) {
   }
   effectFn.deps.length = 0
 }
+
+const jobQueue = new Set()
+const p = Promise.resolve()
+
+let isFlushing = false
+function flushJob() {
+  if (isFlushing) {
+    return
+  }
+  isFlushing = true
+  p.then(() => {
+    jobQueue.forEach(job => job())
+  }).finally(() => {
+    isFlushing = false
+  })
+}
+
+effect(() => {
+  console.log(obj.foo)
+}, {
+  scheduler(fn) {
+    jobQueue.add(fn)
+    flushJob()
+  }
+})
+obj.foo++
+obj.foo++
+
 // effect(() => {
 //   obj.foo = obj.foo + 1
 // })
@@ -82,10 +110,10 @@ function cleanup(effectFn) {
 //   })
 //   const temp = obj.foo
 // })
-effect(() => console.log('foo', obj.foo), {
-  scheduler(fn) {
-    setTimeout(fn)
-}})
-obj.foo++
-console.log('end')
+// effect(() => console.log('foo', obj.foo), {
+//   scheduler(fn) {
+//     setTimeout(fn)
+// }})
+// obj.foo++
+// console.log('end')
 
